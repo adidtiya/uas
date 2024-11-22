0ce5680cb7b727f24360e9a19b25787f789de7a7
Galang <Galangafdalahharsa88@gmail.com>
2024-11-22 10:13:38 +0700

    fix bug

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
 exports.renderHomePage = (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
+    res.sendFile(path.join(__dirname, '../../../views/index.html'));
 };
